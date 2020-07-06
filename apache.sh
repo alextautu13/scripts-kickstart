1386303a642ffd0a2972ce61e0de87d1948649d9
Alex Tautu <36303736+alextautu13@users.noreply.github.com>
2020-07-07 00:56:04 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -10,15 +10,14 @@ systemctl enabled httpd
 
 fi
 
+
 # add dummy certs 
-
-#determine Public IP 
-public_ip=$(wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4)
-
 cd /etc/pki/tls/certs ; sudo ./make-dummy-cert localhost.crt
 sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN PRIVATE KEY-----/,/-----END PRIVATE KEY-----/p' | sudo tee /etc/pki/tls/private/localhost.key
 sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | sudo tee /etc/pki/tls/certs/cert.crt
 sed -i 's/localhost.crt/cert.crt/g' /etc/httpd/conf.d/ssl.conf
+
+public_ip=$(wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4)
 
 tee /etc/httpd/conf.d/redirect.conf <<EOF
 <VirtualHost *:80>
@@ -43,5 +42,5 @@ tee /var/www/html/index.html <<EOF
 </html>
 EOF
 
-#determine Public IP 
-public_ip=$(wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4)
+
+
