0d2bb029b72a219626af2f436d349c52e464857e
Alex Tautu <36303736+alextautu13@users.noreply.github.com>
2020-07-07 00:52:55 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -12,6 +12,9 @@ fi
 
 # add dummy certs 
 
+#determine Public IP 
+public_ip=$(wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4)
+
 cd /etc/pki/tls/certs ; sudo ./make-dummy-cert localhost.crt
 sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN PRIVATE KEY-----/,/-----END PRIVATE KEY-----/p' | sudo tee /etc/pki/tls/private/localhost.key
 sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | sudo tee /etc/pki/tls/certs/cert.crt
