e0df12cdc2eb796d640c2a9935d5f54a0f8cd4b5
Alex Tautu <36303736+alextautu13@users.noreply.github.com>
2020-07-08 15:06:23 +0100

    Update and rename apache.sh to apache_setup.sh

--- apache_setup.sh
+++ apache_setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -v
 
-
-# configure httpd 
-
+configure () {
 systemctl start httpd
 systemctl enable httpd
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
-find /var/www, -type, d, -exec, chmod, 2775, {}, \; 
-find /var/www, -type, f, -exec, chmod, 0664, {}, \; 
+find /var/www -type d -exec chmod 750 {} \;
+find /var/www -type f -exec chmod 640 {} \;
  
 
 # check if apache is enabled 
@@ -18,11 +16,12 @@ is_enabled=$(systemctl is-enabled http)
 
 if [ "$is_enabled" = "disabled" ]
 then 
-systemctl start httpd
 systemctl enable httpd
 
 fi
+}
 
+add_cert () {
 
 # add dummy certs 
 cd /etc/pki/tls/certs ; sudo ./make-dummy-cert localhost.crt
@@ -30,13 +29,12 @@ sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN PRIVATE KEY
 sudo cat /etc/pki/tls/certs/localhost.crt | sudo sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | sudo tee /etc/pki/tls/certs/cert.crt
 sed -i 's/localhost.crt/cert.crt/g' /etc/httpd/conf.d/ssl.conf
 
-
-#/usr/lib/systemd/system/httpd.service
-#PUBLIC_IPV4=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-ipv4)
-#PUBLIC_HOSTNAME=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/public-hostname)
+}
 
 
-tee /usr/lib/systemd/system/httpd.service  &>/dev/null <<EOF
+http_service () {
+
+cat << 'EOF' > /usr/lib/systemd/system/httpd.service
 
 # See httpd.service(8) for more information on using the httpd service.
 
@@ -74,38 +72,51 @@ PrivateTmp=true
 
 [Install]
 WantedBy=multi-user.target
+
 EOF
 
-# add http to https redirect 
- 
-tee /etc/httpd/conf.d/redirect.conf  &>/dev/null <<EOF
-<VirtualHost *:80>
-ServerName ${PUBLIC_IPV4}
-Redirect permanent / https://${PUBLIC_IPV4}/
-</VirtualHost>
-<VirtualHost *:80>
-ServerName ${PUBLIC_HOSTNAME}
-Redirect permanent / https://${PUBLIC_HOSTNAME}/
-</VirtualHost>             
-EOF 
+}
 
 
-
-# create index.html
-tee /var/www/html/index.html &>/dev/null <<EOF
+index_file () {
+cat << 'EOF' > /var/www/html/index.html 
+tee /var/www/html/index.html 
 <html>
  <head>
   <title>Hello AWS Test Page </title>
  </head>
  <body>
- <p>Hello AWS!</p>
+ <h1>Hello AWS!</h1>
  </body>
 </html>
 EOF
 
+}
+
+redirect () {
+cat << 'EOF' > /etc/httpd/conf.d/redirect.conf 
+<VirtualHost *:80>
+ServerName ${PUBLIC_IPV4}
+Redirect permanent / https://${PUBLIC_IPV4}/
+</VirtualHost>
+
+<VirtualHost *:80>
+ServerName ${PUBLIC_HOSTNAME}
+Redirect permanent / https://${PUBLIC_HOSTNAME}/
+</VirtualHost>             
+EOF
+
+}
+
+#call functions
+
+configure
+add_cert
+http_service
+redirect
+index_file 
 
 # restart reload service change and restart httpd 
 systemctl daemon-reload
-systemctl restart httpd 
-
-
+systemctl stop httpd 
+systemctl start httpd 
